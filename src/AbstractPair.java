9a0d7c57aed15b2cd06b5fc291289211387050b5
Mariiashtog <shtohryn.mariia@chnu.edu.ua>
2025-11-13 09:11:22 +0200

    add code in file //

--- src/AbstractPair.java
+++ src/AbstractPair.java
@@ -1,2 +1,24 @@
-public class AbstractPair {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+abstract class AbstractPair {
+    protected double a;
+    protected double b;
+
+    public AbstractPair(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public abstract AbstractPair add(AbstractPair var1);
+
+    public abstract AbstractPair subtract(AbstractPair var1);
+
+    public abstract AbstractPair multiply(AbstractPair var1);
+
+    public abstract String toString();
+
+    public abstract boolean equals(Object var1);
 }
