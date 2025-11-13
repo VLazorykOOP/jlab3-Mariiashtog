9db78f1a996fa768c51ee1e9155cf0f4740fc65a
Mariiashtog <shtohryn.mariia@chnu.edu.ua>
2025-11-13 09:17:32 +0200

    add firl Pair

--- src/Pair.java
+++ src/Pair.java
@@ -1,2 +1,16 @@
-public class Pair {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+interface Pair {
+    Pair add(Pair var1);
+
+    Pair subtract(Pair var1);
+
+    Pair multiply(Pair var1);
+
+    Pair divideByInt(int var1);
+
+    String toString();
 }
